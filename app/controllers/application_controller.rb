1c19b422d2862b4e850ba4793f30f7d32af5117f
engeshra <eng.eshra@gmail.com>
2012-05-08 02:50:20 +0300

    Update app/controllers/application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   private
-
+  def getit
+  end
   def current_user
     #flash[:notice] = "in app action"+@current_user.name
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
